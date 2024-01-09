7112320e0b58935669a2cf44626c46f424a1a557
Leo Wang <54620490+wyy-code@users.noreply.github.com>
2024-01-09 16:32:18 +0800

    Update mian.py

--- mian.py
+++ mian.py
@@ -23,40 +23,37 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 # choose the base model and dataset
-model = ["Dual_AMN", "TransEdge", "RSN"][2]
+model = ["Dual_AMN", "RSN"][1]
 dataset = ["DBP_ZH_EN/", "DBP_JA_EN/", "DBP_FR_EN/", "SRPRS_FR_EN/", "SRPRS_DE_EN/"][3]
 
 if "DBP" in dataset:
-    path = "./EA_datasets/" + ("sharing/" if model == "TransEdge" else "mapping/") + dataset + "0_3/"
+    path = "./EA_datasets/" + "mapping/" + dataset + "0_3/"
 else:
-    path = "./EA_datasets/" + ("sharing/" if model == "TransEdge" else "mapping/") + dataset
+    path = "./EA_datasets/" + "mapping/" + dataset
 
 train_pair, test_pair = load_aligned_pair(path)
 
-if model != "TransEdge":
-    if model == "RSN":
-        emb_path = "Embeddings/RSN/%s" % dataset
-        ent_emb = tf.cast(np.load(emb_path + "ent_emb.npy"), "float32")
-        ent_dic, rel_dic = json.load(open(emb_path + "ent_id2id.json")), json.load(open(emb_path + "rel_id2id.json"))
-        sort_key = []
-        sort_value = []
-        for i in range(ent_emb.shape[0]):
-            sort_key.append(i)
-        for i in sort_key:
-            sort_value.append(int(ent_dic[str(i)]))
+if model == "RSN":
+    emb_path = "Embeddings/RSN/%s" % dataset
+    ent_emb = tf.cast(np.load(emb_path + "ent_emb.npy"), "float32")
+    ent_dic, rel_dic = json.load(open(emb_path + "ent_id2id.json")), json.load(open(emb_path + "rel_id2id.json"))
+    sort_key = []
+    sort_value = []
+    for i in range(ent_emb.shape[0]):
+        sort_key.append(i)
+    for i in sort_key:
+        sort_value.append(int(ent_dic[str(i)]))
 
-        ent_emb = ent_emb.numpy()[sort_value]
-        ent_emb = tf.cast(ent_emb, "float32")
+    ent_emb = ent_emb.numpy()[sort_value]
+    ent_emb = tf.cast(ent_emb, "float32")
 
-        print("RSN")
-    else:
-        ent_emb = tf.cast(np.load("Embeddings/Dual_AMN/%sent_emb.npy" % dataset), "float32")
-        print("Dual_AMN")
+    print("RSN")
 else:
-    ent_emb = tf.cast(np.load("Embeddings/TransEdge/%sent_embeds.npy" % dataset), "float32")
-    print("TransEdge")
+    ent_emb = tf.cast(np.load("Embeddings/Dual_AMN/%sent_emb.npy" % dataset), "float32")
+    print("Dual_AMN")
 
 # decoding algorithm
+# Triple Feature Propagation based on the entity embedding
 node_size, rel_size, ent_tuple, triples_idx, ent_ent, ent_ent_val, rel_ent, ent_rel = load_graph(path)
 
 print("Begin to Triple Feature Propagate:")
